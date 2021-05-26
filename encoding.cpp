53888fc8ec73a4abc3ce9682b175e36d6ea73545
kunjpatel1402 <75675988+kunjpatel1402@users.noreply.github.com>
2021-05-26 21:55:47 +0530

    Update encoding.cpp

--- encoding.cpp
+++ encoding.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 #include "stack.h"
 #include "queue.h"
+#include <bitset>
 
 using namespace std;
 
@@ -65,10 +66,12 @@ unsigned int bin2uint(intqueue* q){
 }
 
 void count_character_frequencies(string filename, unsigned long long int* freq_of_character, unsigned long long int* freq_of_next_line,unsigned long long int* total_frequency){
-    ifstream fin(filename);
+    ifstream fin;
+    fin.open(filename,ios::binary);
     string s;
     while (true){
         getline(fin,s);
+        cout<<s<<endl;
         (*freq_of_next_line)++;
         (*total_frequency)++;
         if (fin.eof()) break;
@@ -76,18 +79,78 @@ void count_character_frequencies(string filename, unsigned long long int* freq_o
             freq_of_character[s[i]]++;
             (*total_frequency)++;
         }
+        //cout<<s<<endl;
     }
+    cout<<fin.eof();
     fin.close();
+}
+
+void compress_file_to(string original_file_name, string compressed_file_name,string* binary_code_for_character){
+    ifstream fin(original_file_name,ios::binary);
+    ofstream fout(compressed_file_name,ios::binary);
+    string s;
+    unsigned int d;
+    intqueue q;
+    while(true){
+        getline(fin,s);
+        if (fin.eof()) break;
+        cout<<s<<endl;
+        for (unsigned long long int i = 0; i < s.length();i++){
+            add_to_queue(&q,binary_code_for_character[s[i]]);
+            if (q.get_size() >= 32){
+                d = bin2uint(&q);
+                //fout<<d<<endl;
+                fout.write((char*) &d,sizeof(d));
+            }
+        }
+        add_to_queue(&q,binary_code_for_character['\n']);
+        if (q.get_size() >= 32){
+                d = bin2uint(&q);
+                //fout<<d<<endl;
+                fout.write((char*) &d,sizeof(d));
+        }
+    }
+    //q.print();
+    do{
+        d = bin2uint(&q);
+        //fout<<d<<endl;
+        fout.write((char*) &d,sizeof(d));
+    }while(!q.empty());
+    fout.close();
+    fin.close();
+    cout<<q.empty()<<endl;
 }
 
 int main(){
     string binary_code_for_character[256]={""};
     unsigned long long int freq_of_character[256] = {0},freq_of_next_line = 0,total_frequency = 0;
-    count_character_frequencies("test.txt",freq_of_character,&freq_of_next_line,&total_frequency);
+    cout<<"1"<<endl;
+    count_character_frequencies("C:\\Users\\Kunj R. Patel\\Desktop\\codes\\c++\\assignment\\huffmann coding\\test.txt",freq_of_character,&freq_of_next_line,&total_frequency);
+    cout<<"2"<<endl;
+    stack s;
     for (unsigned long long int i = 0; i < 256; i++){
-        cout<<(char)i<<" "<<freq_of_character[i]<<endl;
+        //cout<<(char)i<<" "<<freq_of_character[i]<<endl;
+        if (freq_of_character[i]){
+            s.push(create_node((char)i,freq_of_character[i]*1.0/total_frequency,false));
+        }
     }
-    cout<<freq_of_next_line<<endl;
-    cout<<total_frequency<<endl;
+    s.push(create_node('\n',freq_of_next_line*1.0/total_frequency,false));
+   //cout<<total_frequency<<endl;
+    printInorder(create_tree(&s),"",binary_code_for_character);
+    /*for (int i = 0; i < 256; i++){
+        if (binary_code_for_character[i]!= ""){
+            cout<<(char)i<<" "<<binary_code_for_character[i]<<endl;
+        }
+    }*/
+    compress_file_to("C:\\Users\\Kunj R. Patel\\Desktop\\codes\\c++\\assignment\\huffmann coding\\test.txt","compressed.dat",binary_code_for_character);
+    /*ifstream fin("compressed.dat",ios::binary);
+    unsigned int u;
+    //bool t;
+    while(true){
+        fin.read((char*) &u, sizeof(u));
+        if (fin.eof()) break;
+        bitset<32> x(u);
+        cout<<x;
+    }*/
     return 0;
 }
