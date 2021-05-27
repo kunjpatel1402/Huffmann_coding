79b9d2b6446c0214b1a85158ed74dac8103a8f78
Achyut-1412 <79002869+Achyut-1412@users.noreply.github.com>
2021-05-27 08:03:50 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,19 @@
 #include <iostream>
 #include<fstream>
+#include <bitset>
 #include<bits/stdc++.h>
 
 using namespace std;
+
+ char *node_data;
 class Graph
 {
 public:
    // map<int, bool> visited;
     map<int, list<int>> adj;
-
-    char *node_data;
-
-
-    Graph(int n){
-
-        node_data = new char[n+1];
-        node_data[0]='*';
-        int t;
-        for(int i=1;i<=n;i++)
-        {
-            readfile >> t >>"\n";
-            readfile >>node_data[t];
-
-        }
-    }
     void addEdge(int v, int w);
-    void decoding(string es);
-    string input();//remaining
+    void decoding();
+    //string input();//remaining
 };
 
 
@@ -35,30 +22,54 @@ void Graph::addEdge(int v, int w)
     adj[v].push_back(w);
 
 }
-void Graph::decoding(string &es)
+void Graph::decoding()
 {
-    string ds="";
+
     int v=1;
-    for(int i=0;i<es.length();i++)
+    ofstream MyFile("decoded.txt");
+    cout<<"Please enter the name of compressed file: (with .dat extention)";
+    string inp;
+    cin>>inp;
+    ifstream fin("inp",ios::binary);
+    //Myfile will write the decoded data in the file whereas comressed file is the file from where we get the encoded data
+    unsigned int u;
+    /*now we will traverse the tree iteratively untill we read the whole file in each traversal whenever we read 1 we will move to the left child and 
+    we move to the right child if we read 0 and if we reach at some leaf node we write it's char data to our decompressed file and again start traversing
+    from the root*/
+    while(1)
     {
-        if(es[i]=='1')
+        fin.read((char *)&u,sizeof(u));
+        if(fin.eof())
+            break;
+        else
         {
-          auto it=adj[v].begin() ;
-          v=*it;
-        }
-        else if(es[i]=='0')
-        {
-            auto it=adj[v].end()-1;
-            v = *it;
-        }
-        if(adj[v].begin()==adj[v].end())
-        {
-            ds = ds + node_data[v];
-            v=1;
+           bitset<32> x(u);
+           for(int i=1;i<=32;i++)
+          {
+            if(x[i]==1)
+           {
+              auto it=adj[v].begin() ;
+              v=*it;
+           }
+           else if(x[i]==0)
+            {
+             auto it=adj[v].begin();
+             it++;
+             v = *it;
+            }
+            if(adj[v].begin()==adj[v].end())
+            {
+              MyFile<<node_data[v];
+               v=1;
+            }
+         }
 
         }
-
     }
+
+    MyFile.close();
+    fin.close();
+
 }
 
 
@@ -70,21 +81,33 @@ int main()
     cout<<"Please enter the file name: ";
     cin>>s;
     ifstream readfile(s);
+    //this will start reading the file which contains data of nodes,edges and  char each node has
     int n;
-    readfile >> n >>"\n";
-    Graph g(n);
+    readfile >> n ;
+    Graph g;
+     node_data = new char[n+1];
+        node_data[0]='*';
+        int t;
+        for(int i=1;i<=n;i++)
+        {
+            readfile >> t;
+            readfile >>node_data[t];
+
+        }
     for(int i=0;i<n-1;i++)
     {
         int a,b;
         readfile >> a;
-        readfile >> b>>"\n";
-        g.addEdge(a,b);
+        readfile >> b;
+        g.addEdge(a,b);//making tree using edge data
 
     }
-    string es;
-    es = g.input();
+    readfile.close();
+    /*string es;
+    es = g.input();*/
 
-    g.decoding(es)
-
+    g.decoding();
+    cout<<"Decomressed file is ready! please check it out"<<"\n";
+    cout<<"Thank you for using our software"<<"\n";
     return 0;
 }
