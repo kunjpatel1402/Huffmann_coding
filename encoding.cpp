fe4f227a4a232ff5d3573533b3c1f051e68fcae4
kunjpatel1402 <75675988+kunjpatel1402@users.noreply.github.com>
2021-06-05 17:29:55 +0530

    Update encoding.cpp

--- encoding.cpp
+++ encoding.cpp
@@ -3,29 +3,35 @@
 #include <math.h>
 #include "stack.h"
 #include "queue.h"
-#include <bitset>
 
 using namespace std;
 
-void printInorder(node* root,string s,string* binary_code_for_the_character){
+void printPreorder(node* root,string s,string* binary_code_for_the_character,int* nodes_in_tree){
     if (root == NULL)
         return;
-    printInorder(root->left,(s + "1"),binary_code_for_the_character);
-    printInorder(root->right,(s + "0"),binary_code_for_the_character);
+    binary_code_for_the_character[root->symbol] = s;
+    (*nodes_in_tree)++;
+    //cout<<*nodes_in_tree<<endl;
+    root->node_number = *nodes_in_tree;
+    printPreorder(root->left,(s + "1"),binary_code_for_the_character,nodes_in_tree);
+    printPreorder(root->right,(s + "0"),binary_code_for_the_character,nodes_in_tree);
     //print_node(root);
     //cout<<"\n"<<s<<endl;
-    binary_code_for_the_character[root->symbol] = s;
 }
 
 node* create_tree(stack* s){
-    int pause;
+    int pause = 0;
     (*s).sort();
     //(*s).print();
     while(true){
+        if ((*s).get_size() < 2){
+            cout<<"ERROR"<<endl;
+        }
         node* c1 = (*s).pop();
         node* c2 = (*s).pop();
         node* p = new node;
-        p = create_node('\0',c1->probablity+c2->probablity,true);
+        pause += 2;
+        p = create_node('\0',c1->probablity+c2->probablity,true,c1,c2,nullptr);
         p->left = c1;
         p->right = c2;
         c1->parent = p;
@@ -39,6 +45,7 @@ node* create_tree(stack* s){
             break;
         }
     }
+    cout<<pause<<endl;
     return ((*s).pop());
 }
 
@@ -100,43 +107,106 @@ void compress_file_to(string original_file_name, string compressed_file_name,str
             if (q.get_size() >= 32){
                 d = bin2uint(&q);
                 //fout<<d<<endl;
-                fout.write((char*) &d,sizeof(d));
+                fout.write((char*)&d,sizeof(d));
             }
         }
         add_to_queue(&q,binary_code_for_character['\n']);
         if (q.get_size() >= 32){
                 d = bin2uint(&q);
                 //fout<<d<<endl;
-                fout.write((char*) &d,sizeof(d));
+                fout.write((char*)&d,sizeof(d));
         }
     }
     //q.print();
     do{
         d = bin2uint(&q);
         //fout<<d<<endl;
-        fout.write((char*) &d,sizeof(d));
+        fout.write((char*)&d,sizeof(d));
     }while(!q.empty());
     fout.close();
     fin.close();
     cout<<q.empty()<<endl;
 }
 
+void write_symbols(fstream& fout,node* n){
+    fout.seekg(0,ios::end);
+    if (n != NULL){
+        //fout.write((char*) &(n->node_number),sizeof(n->node_number));
+        fout<<n->node_number<<" ";
+        if (!n->dummy){
+            //fout.write((char*) &(not_dummy),sizeof(not_dummy));
+            int ascii_code = n->symbol;
+            fout<<ascii_code<<endl;
+            //fout.write((char*) &(ascii_code),sizeof(ascii_code));
+        }
+        else{
+            int not_dummy = 0;
+            //fout.write((char*) &(not_dummy),sizeof(not_dummy));
+            fout<<not_dummy<<endl;
+        }
+        write_symbols(fout,n->left);
+        write_symbols(fout,n->right);
+    }
+    else{
+        return;
+    }
+}
+void write_edges(fstream& fout,node* n){
+    if ((n->right != NULL)&&(n->left!=NULL)){
+        /*int num = n->node_number;
+        fout.write((char*) &(num),sizeof(num));
+        cout<<num<<" ";
+        num = n->left->node_number;
+        fout.write((char*) &(num),sizeof(num));
+        cout<<num<<endl;
+        num = n->node_number;
+        fout.write((char*) &(num),sizeof(num));
+        cout<<num<<" ";
+        num = n->right->node_number;
+        fout.write((char*) &(num),sizeof(num));
+        cout<<num<<endl;*/
+        fout<<n->node_number<<" "<<n->left->node_number<<endl;
+        fout<<n->node_number<<" "<<n->right->node_number<<endl;
+        write_edges(fout,n->left);
+        write_edges(fout,n->right);
+    }
+    else{
+        //cout<<"node number:"<<n->node_number<<" "<<n->symbol<<"|"<<endl;
+        return;
+    }
+}
+
+void write_tree_to(fstream& fout,node* n,int* nodes_written){
+    //fout<<(*nodes_written)<<endl;
+    int no = *nodes_written;
+    //fout.write((char*) &no,sizeof(int));
+    fout<<no<<endl;
+    //fout<<"symbols"<<endl;
+    write_symbols(fout,n);
+    //fout<<"edges"<<endl;
+    cout<<"file"<<endl;
+    write_edges(fout,n);
+}
+
 int main(){
+    cout<<"\n started";
     string binary_code_for_character[256]={""};
     unsigned long long int freq_of_character[256] = {0},freq_of_next_line = 0,total_frequency = 0;
-    cout<<"1"<<endl;
+    //cout<<"1"<<endl;
     count_character_frequencies("C:\\Users\\Kunj R. Patel\\Desktop\\codes\\c++\\assignment\\huffmann coding\\test.txt",freq_of_character,&freq_of_next_line,&total_frequency);
-    cout<<"2"<<endl;
+    //cout<<"2"<<endl;
     stack s;
     for (unsigned long long int i = 0; i < 256; i++){
         //cout<<(char)i<<" "<<freq_of_character[i]<<endl;
         if (freq_of_character[i]){
-            s.push(create_node((char)i,freq_of_character[i]*1.0/total_frequency,false));
+            s.push(create_node((char)i,freq_of_character[i]*1.0/total_frequency,false,nullptr,nullptr,nullptr));
         }
     }
     s.push(create_node('\n',freq_of_next_line*1.0/total_frequency,false));
    //cout<<total_frequency<<endl;
-    printInorder(create_tree(&s),"",binary_code_for_character);
+    node* root_of_tree = create_tree(&s);
+    int nodes_in_tree = 0;
+    printPreorder(root_of_tree,"",binary_code_for_character,&nodes_in_tree);
     /*for (int i = 0; i < 256; i++){
         if (binary_code_for_character[i]!= ""){
             cout<<(char)i<<" "<<binary_code_for_character[i]<<endl;
@@ -152,5 +222,19 @@ int main(){
         bitset<32> x(u);
         cout<<x;
     }*/
+    cout<<nodes_in_tree<<endl;
+    cout<<root_of_tree->probablity<<endl;
+    fstream fout("compressed_tree.dat",ios::out);
+    write_tree_to(fout,root_of_tree,&nodes_in_tree);
+    fout.close();
+    /*int pause;
+    ifstream fin("compressed.dat",ios::binary);
+    unsigned int n;
+    while(!fin.eof()){
+        fin.read((char*) &n,sizeof(n));
+        cout<<n<<endl;
+        //cout<<":";cin>>pause;
+    }
+    fin.close();*/
     return 0;
 }
