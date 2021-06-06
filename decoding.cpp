6ff89ae7f92579c0a9c2f8d7e3495de1cd0b7d0d
kunjpatel1402 <75675988+kunjpatel1402@users.noreply.github.com>
2021-06-06 10:57:13 +0530

    Update decoding.cpp

--- decoding.cpp
+++ decoding.cpp
@@ -5,6 +5,27 @@
 
 using namespace std;
 
+void progress_bar(int completed){
+    for (int i = 0; i < completed;i++){
+        cout<<"|";
+    }
+    for (int i = completed; i < 30; i++){
+        cout<<" ";
+    }
+    cout<<"|";
+    cout<<" decompressing file";
+    //for(int i =0 ; i < 1000000; i++);
+    cout<<"\r";
+}
+
+void clear(){
+    cout<<"\r";
+    for (int i = 0; i < 50; i++){
+        cout<<" ";
+    }
+    cout<<"\n->file decompressed"<<endl;
+}
+
  char *node_data;
 class Graph
 {
@@ -15,8 +36,6 @@ public:
     void decoding();
     
 };
-
-
 void Graph::addEdge(int v, int w)
 {
     adj[v].push_back(w);
@@ -26,60 +45,25 @@ void Graph::decoding()
 {
 
     int v=1;
-    ofstream MyFile("decoded.txt");
-    cout<<"Please enter the name of compressed file: (with .dat extention)"<<endl;
     string inp;
-    //cin>>inp;
-    ifstream fin("compressed.dat",ios::binary);
-    //Myfile will write the decoded data in the file whereas comressed file is the file from where we get the encoded data
+    cout<<"\n\n enter name for decompressed file(with .txt extention):\n";
+    cin>>inp;
+    ofstream MyFile(inp);
+    cout<<"Please enter the name of compressed file: (with .dat extention)"<<endl;
+    cin>>inp;
+    ifstream fin(inp,ios::binary);
     unsigned int u;
-    /*now we will traverse the tree iteratively untill we read the whole file in each traversal whenever we read 1 we will move to the left child and 
-    we move to the right child if we read 0 and if we reach at some leaf node we write it's char data to our decompressed file and again start traversing
-    from the root*/
-    /*while(1)
-    {
-        fin.read((char *)&u,sizeof(u));
-        cout<<u<<endl;
-        if(fin.eof())
-            break;
-        else
-        {
-           bitset<32> x(u);
-           for(int i=1;i<=32;i++)
-          {
-            if(x[i]==1)
-           {
-              auto it=adj[v].begin() ;
-              v=*it;
-           }
-           else if(x[i]==0)
-            {
-             auto it=adj[v].begin();
-             it++;
-             v = *it;
-            }
-            if(adj[v].begin()==adj[v].end())
-            {
-              MyFile<<node_data[v];
-               v=1;
-            }
-         }
-
-        }
-    }*/
     if (!fin){
         cout<<"cannot open file";
         return ;
     }
     else{
+        int progress = 0;
         unsigned int u,pause;
         while(true){
             fin.read((char*) &u,sizeof(u));
-            //cout<<u<<endl;
-            //cout<<":";cin>>pause;
             if (fin.eof())  break;
             bitset<32> x(u);
-            cout<<x;
             for(int i=31;i>=0;i--)
             {
                 if(x[i]==1)
@@ -96,12 +80,14 @@ void Graph::decoding()
                 if(adj[v].begin()==adj[v].end())
                 {
                     MyFile<<node_data[v];
-                    //cout<<node_data[v]<<" "<<v<<endl;;
                     v=1;
                 }
             }
+            progress = ++progress%30;
+            progress_bar(progress);
         }
     }
+    clear();
     MyFile.close();
     fin.close();
 }
@@ -112,28 +98,23 @@ int main()
 {
     string s;
 
-    cout<<"Please enter the file name: "<<endl;
-    //cin>>s;
-    ifstream readfile("compressed_tree.dat",ios::binary);
-    //this will start reading the file which contains data of nodes,edges and  char each node has
+    cout<<"Please enter the tree file name: "<<endl;
+    cin>>s;
+    ifstream readfile(s,ios::binary);
     int n;
     readfile >> n ;
-    cout<<n<<endl;
     Graph g;
      node_data = new char[n+1];
- // ane aiya '\0' thi init pan kari de
-    node_data[0]='*';
     for (int i = 0; i < n+1; i++){
         node_data[0]='\0';
     }
     int t,sym;
+    cout<<"\n->creating character tree";
     while(!readfile.eof()){
         for(int i=1;(i<=n)&&(!readfile.eof());i++)
         {
             readfile >> t;
-        // aiya ek if muk jo dummy na hoy to j symbol read thay baki nai
             readfile >>sym;
-            cout<<t<<" "<<(char)sym<<endl;
             node_data[t] = (char)sym;
 
         }
@@ -142,16 +123,12 @@ int main()
             int a,b;
             readfile >> a;
             readfile >> b;
-            g.addEdge(a,b);//making tree using edge data
-            //cout<<a<<" "<<b<<endl;
+            g.addEdge(a,b);
         }
     }
     readfile.close();
-    /*string es;
-    es = g.input();*/
-
     g.decoding();
-    cout<<"Decomressed file is ready! please check it out"<<"\n";
+    cout<<"\nDecomressed file is ready! please check it out"<<"\n";
     cout<<"Thank you for using our software"<<"\n";
     return 0;
 }
